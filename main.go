8ca9e7062bd8d8f658465a7ea73f549212b0a8eb
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2024-03-17 13:23:13 +0100

    :sparkles: add lambda api proxy

--- main.go
+++ main.go
@@ -1,33 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	if request.HTTPMethod != http.MethodGet {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusMethodNotAllowed,
-			Body:       "Method Not Allowed",
-			Headers: map[string]string{
-				"Content-Type": "text/plain",
-			},
-		}, nil
-	}
-
-	shorten_url := request.PathParameters["shorten_url"]
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusPermanentRedirect,
-		Headers: map[string]string{
-			"Location": shorten_url,
-		},
-	}, nil
-}
-
 func main() {
-	lambda.Start(handler)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Root\n")
+	})
+
+	lambda.Start(httpadapter.New(mux).ProxyWithContext)
 }
