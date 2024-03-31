35c8cd5bdfb824530e74956be0b615829eb23e73
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2024-03-31 19:44:18 +0200

    :sparkles: add simple function

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	"os"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+func GetShortenedUrl(w http.ResponseWriter, r *http.Request) {
+	shortenedUrl := r.PathValue("shortenedUrl")
+	fmt.Fprintf(w, "<h1>You tried to access the following url: %s</h1>", shortenedUrl)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", Handler)
+	mux.HandleFunc("/{shortenedUrl}", GetShortenedUrl)
 
 	port := os.Getenv("PORT")
 	if port == "" {
